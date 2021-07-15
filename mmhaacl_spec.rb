6a663d0d70c22009c1cb6042b0ade798ccf3f304
Adam Cathersides <adamcathersides@gmail.com>
2021-07-15 10:14:08 +0000

    test data and test

--- mmhaacl_spec.rb
+++ mmhaacl_spec.rb
@@ -1,3 +1,8 @@
 require 'mmhaacl.rb'
 
-RSpec.describe 
+RSpec.describe get_geoname_ids do
+  It 'retreives ids from the csv' do
+    ids = get_geoname_ids('city_test_data.csv')
+    expect(ids).to equal([])
+  end
+end
